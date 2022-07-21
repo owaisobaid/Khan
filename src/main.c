fa1a0be1e2285e03f0180435443f7417a97ea36c
root <root@localhost>
2022-07-21 09:56:34 +0530

    auto publish 1658377593835

---
+++ src/main.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+    int main()
+  {
+    int a;
+    int b;
+{
+  printf("Enter value of a and b\n");
+  scanf("%i%i",&a,&b);
+  
+}
+  
+if(a>b) 
+
+{   
+    printf("That is good");
+}
+  else
+{
+  printf("wrong");
+}
+  
+    
+  
+  }
